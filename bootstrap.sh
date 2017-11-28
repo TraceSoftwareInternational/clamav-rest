ba3940fd070fed4770da84cd9c9add77743c1b7a
David Ducatel <dducatel@trace.fr>
2017-11-28 11:49:40 +0100

    Add docker deployment
    ref to #1

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,3 @@ echo "Max request size: $maxrequestsize"
 
 # start in background
 java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port --clamd.maxfilesize=$maxfilesize --clamd.maxrequestsize=$maxrequestsize
-
-
-
-
